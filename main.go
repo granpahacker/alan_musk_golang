05be85d0e05ade7322388607d4d01d7234465c39
granpahacker <granparobloxlover@gmail.com>
2023-02-11 18:08:09 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,22 @@
+// Copyright (C) Oleg Lysiak - All Rights Reserved
+package main
+
+import (
+	"fmt"
+	"golang-discord-bot/bot"
+	"golang-discord-bot/config"
+)
+
+func main() {
+	err := config.ReadConfig()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	bot.Start()
+
+	<-make(chan struct{})
+	return
+}
