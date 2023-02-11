dc3062a4340e5a081dab8dfd7a392c7a22a27092
granpahacker <granparobloxlover@gmail.com>
2023-02-11 19:00:28 +0200

    code update

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: bin/golang-discord-bot
--- main.go
+++ main.go
@@ -5,18 +5,45 @@ import (
 	"fmt"
 	"golang-discord-bot/bot"
 	"golang-discord-bot/config"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	err := config.ReadConfig()
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	// check for errors
 
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
 
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("templates/*.tmpl.html")
+	// router.Static("/static", "static")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	})
+
+	// start bot
 	bot.Start()
 
+	// start server
+	router.Run(":" + port)
+
 	<-make(chan struct{})
+
 	return
 }
